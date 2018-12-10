cac26e06e7d75a7453aa1ac2dc5c0e4f1c6adbc7
RavenInd <emico.crow@gmail.com>
2018-12-10 20:35:01 +0300

    Updates

---
+++ precache-manifest.b18873db108bb1e8d7961fe7fd6555d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "475564bd76d6f9608a55",
+    "url": "/React-challenge-SPA-dogs/static/js/runtime~main.475564bd.js"
+  },
+  {
+    "revision": "3648cfaa830d28e3ebf6",
+    "url": "/React-challenge-SPA-dogs/static/js/main.3648cfaa.chunk.js"
+  },
+  {
+    "revision": "67fa1f7b3d362e25012b",
+    "url": "/React-challenge-SPA-dogs/static/js/1.67fa1f7b.chunk.js"
+  },
+  {
+    "revision": "67fa1f7b3d362e25012b",
+    "url": "/React-challenge-SPA-dogs/static/css/1.f42d020e.chunk.css"
+  },
+  {
+    "revision": "b4036be90e9eae6f4e4b3704c86519ec",
+    "url": "/React-challenge-SPA-dogs/index.html"
+  }
+];
